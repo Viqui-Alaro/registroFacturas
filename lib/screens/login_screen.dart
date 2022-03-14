350b651fad8a0ab7f5810867dbc9d5be5ba82459
Viqui Alaro <viquialaro241@gmail.com>
2022-03-13 23:29:56 -0400

    Implementacion de formulario de autentificacion

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,7 +19,7 @@ class LoginScreen extends StatelessWidget {
                     SizedBox(height: 10,),
                     Text('SIAT EN TUS MANOS', style: Theme.of(context).textTheme.headline4,),
                     SizedBox(height: 30),
-                    Text('Viqui')
+                    _LoginForm()
 
                   ],
                 ),
@@ -31,4 +31,40 @@ class LoginScreen extends StatelessWidget {
       )
    );
   }
+
 }
+
+
+  class _LoginForm extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        child: Form(
+          // TODO: mantener la referencia al KEY
+          child: Column(
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey,
+                      width: 2
+                    )
+                  ),
+                  hintText: '9199772013',
+                  labelText: 'Codigo Dependiente',
+                  labelStyle: TextStyle(
+                    color: Colors.grey
+                  ),
+                  prefixIcon: Icon(Icons.person,color: Colors.lightBlueAccent)
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+  }
