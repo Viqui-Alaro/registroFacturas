be09c30d9c382275d22e5337e9fca587ae3578a8
Viqui Alaro <viquialaro241@gmail.com>
2022-03-13 22:17:28 -0400

    Icono central y widgets como argumentos

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 
 class AuthBackground extends StatelessWidget {
 
+  final Widget child;
+
+  const AuthBackground({Key? key, 
+  required this.child}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +17,8 @@ class AuthBackground extends StatelessWidget {
       child: Stack(
         children: [
           _BlueBox(),
-          _HeaderIcon()
+          _HeaderIcon(),
+          this.child
         ],
       ),
     );
