b0fd0d658469c1920d0eca16b260c5b6075fb7b5
Viqui Alaro <viquialaro241@gmail.com>
2022-03-16 23:29:09 -0400

    mejor consumo de servicio de autentificación

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -8,30 +8,6 @@ import 'package:http/http.dart' as http;
 class AuthService extends ChangeNotifier{
 
 
-
-/*
-  Future<String?> autentificacion(String tipoUsuarioId,String nit, String login, String password) async{
-    final Map<String,dynamic> authData={
-
-      'tipoUsuarioId':tipoUsuarioId,
-      'nit':nit,
-      'login':login,
-      'password':password
-    };
-
-    final url=Uri.https(_baseUrl, '/token/get',{
-      
-    });
-
-    
-
-    final resp = await http.post(url,body: json.encode(authData));
-    final Map<String,dynamic>decodedResp = json.decode(resp.body);
-
-    print(decodedResp);
-  }
-
-*/
   Future<String?> autentificacion(String tipoUsuarioId,String nit, String login, String password) async {
   final response = await http.post(
     Uri.parse('https://siatrest.impuestos.gob.bo:39102/token/get'),
