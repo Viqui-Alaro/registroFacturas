4844419fd73c9b318aac57c036ad09a5d74761e8
Viqui Alaro <viquialaro241@gmail.com>
2022-03-18 00:55:00 -0400

    Implementacion de interfaz de usuario y asi como tambien se realizo mejoras en la autentificacion

--- lib/widgets/page_title.dart
+++ lib/widgets/page_title.dart
@@ -15,7 +15,7 @@ class PageTitle extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
                SizedBox(height: 100,),
-                Text('SIAT en tus manos',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blue)),
+                Text('SIAT en tus manos',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.grey)),
                 SizedBox(height: 40,)
             ],
           ),
