8265b3e9e05e62df2923dfe17370b4c75ed047d1
Viqui Alaro <viquialaro241@gmail.com>
2022-03-26 00:26:57 -0400

    cambios

---
+++ lib/share_preferences/preferences.dart
@@ -0,0 +1,13 @@
+/*
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Preferences {
+
+  static SharedPreferences;
+
+
+
+}
+
+*/
